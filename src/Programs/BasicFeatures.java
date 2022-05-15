688d2663d57490f517bb36777a62d3963e54daaf
shiavaregalla99 <shivaregalla99@gmail.com>
2022-05-15 20:33:39 +0530

    first commit stashing

---
+++ src/Programs/BasicFeatures.java
@@ -0,0 +1,9 @@
+package Programs;
+
+public class BasicFeatures {
+	 String engineCapac;
+	    String Color;
+	    String doors;
+	    String headlights;
+	    String regNo;
+}
---
+++ src/Programs/Class01.java
@@ -0,0 +1,10 @@
+package Programs;
+
+public class Class01 {
+
+	public static void main(String[] args) {
+		BasicFeatures cars= new BasicFeatures();
+
+	}
+
+}
